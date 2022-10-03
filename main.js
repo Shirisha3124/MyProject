350013bce9c170197c074fe6cca8fa1bd2da856b
Shirisha3124 <108503120+Shirisha3124@users.noreply.github.com>
2022-10-02 23:30:28 -0600

    Update main.js

--- main.js
+++ main.js
@@ -79,3 +79,49 @@ let calculation = () => {
 calculation();
 
  
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if (search === undefined) {
+        basket.push({
+            id: selectedItem.id,
+            item: 1, 
+          });
+    } 
+    else {
+        search.item += 1;
+    }
+    
+    update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+    if (search === undefined) return;
+    else if (search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    update(selectedItem.id);
+    basket = basket.filter((x) => x.item !==0);
+    
+   
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+   
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+};
+
+let calculation = () => {
+    let cartIcon = document.getElementById("cart-Amt");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+ 
+calculation();
+
+ 
